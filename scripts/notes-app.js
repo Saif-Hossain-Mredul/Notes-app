44dc50fa8af693bb96dfad6733f3fa5babf22480
Saif Hossain <atonlymesaif@gmail.com>
2021-02-23 15:31:56 +0600

    search functionality added

--- scripts/notes-app.js
+++ scripts/notes-app.js
@@ -1,8 +1,19 @@
 let notes = getNotes() 
+let filters = { 
+    searchText: '', 
 
-renderNotes(notes)
+}
+
+renderNotes(notes, filters)
 
 const searchEl = document.querySelector('input#search-input')
+searchEl.addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+
+    renderNotes(notes, filters)
+
+    console.log(filters.searchText)
+})
 
 document.querySelector('button#add-note').addEventListener('click', function(e) {
     const id = uuidv4()
--- scripts/notes-functions.js
+++ scripts/notes-functions.js
@@ -1,46 +1,50 @@
 function getNotes() {
-    const notes = JSON.parse(localStorage.getItem('notes'))  
-   
-    return notes === null ? [] : notes
+	const notes = JSON.parse(localStorage.getItem('notes'))
+
+	return notes === null ? [] : notes
 }
 
 function saveNotes(notes) {
-    // notes = notes.filter((note) => {
-    //     return note.title !== '' && note.body !== ''
-    // }) 
+	// notes = notes.filter((note) => {
+	//     return note.title !== '' && note.body !== ''
+	// })
 
-    localStorage.setItem('notes', JSON.stringify(notes))
+	localStorage.setItem('notes', JSON.stringify(notes))
 }
 
 function generateNoteDom(note) {
-    const noteEl = document.createElement('a')
-    const titleEl = document.createElement('p')   
-    const bodyEl = document.createElement('p')
+	const noteEl = document.createElement('a')
+	const titleEl = document.createElement('p')
+	const bodyEl = document.createElement('p')
 
-    noteEl.setAttribute('href', `edit-page.html#${note.id}`)
+	noteEl.setAttribute('href', `edit-page.html#${note.id}`)
 
-    titleEl.textContent = note.title 
-    titleEl.classList.add('list-item__title')
-    noteEl.appendChild(titleEl)
+	titleEl.textContent = note.title
+	titleEl.classList.add('list-item__title')
+	noteEl.appendChild(titleEl)
 
-    return noteEl
+	return noteEl
 }
 
-function renderNotes (notes) {
-    const notesEl = document.querySelector('div#notes')
+function renderNotes(notes, filters) {
+	const notesEl = document.querySelector('div#notes')
 
-    notesEl.innerHTML = ''
+	const filteredNotes = notes.filter((note) => {
+		return note.title
+			.toLowerCase()
+			.includes(filters.searchText.toLowerCase())
+	})
 
-    if(notes.length === 0) { 
-        const emptyMessage = document.createElement('p') 
-        emptyMessage.textContent = 'You have no notes to show. Lets create one' 
-        notesEl.appendChild(emptyMessage)
-    } else {
-        notes.forEach(function(note) {
-            const noteEl = generateNoteDom(note)
-            notesEl.appendChild(noteEl)
-        }); 
-    }
+	notesEl.innerHTML = ''
 
-    
+	if (notes.length === 0) {
+		const emptyMessage = document.createElement('p')
+		emptyMessage.textContent = 'You have no notes to show. Lets create one'
+		notesEl.appendChild(emptyMessage)
+	} else {
+		filteredNotes.forEach(function (note) {
+			const noteEl = generateNoteDom(note)
+			notesEl.appendChild(noteEl)
+		})
+	}
 }
