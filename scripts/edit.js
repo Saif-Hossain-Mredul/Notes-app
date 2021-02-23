928b87666ca47e1831caf3402ba48b387263df30
Saif Hossain <atonlymesaif@gmail.com>
2021-02-23 15:32:40 +0600

    formatted

--- scripts/edit.js
+++ scripts/edit.js
@@ -4,35 +4,34 @@ const saveButton = document.querySelector('button#save-button')
 const removeButton = document.querySelector('button#remove-button')
 
 const noteId = location.hash.substring(1)
-let notes = getNotes() 
-
+let notes = getNotes()
 
 let note = notes.find((note) => {
 	return note.id === noteId
 })
 let noteIndex = notes.findIndex((note) => {
-    return note.id === noteId
+	return note.id === noteId
 })
 
-titleEl.addEventListener('input', (e) => { 
-    note.title = e.target.value.trim()
+titleEl.addEventListener('input', (e) => {
+	note.title = e.target.value.trim()
 })
 
 note.body.length > 0
 	? (bodyEl.value = note.body)
-    : (bodyEl.placeholder = 'Add note description')
-    
+	: (bodyEl.placeholder = 'Add note description')
+
 bodyEl.addEventListener('input', (e) => {
-    note.body = e.target.value.trim()
+	note.body = e.target.value.trim()
 })
 
 saveButton.addEventListener('click', function (e) {
-    saveNotes(notes) 
-    location.assign('index.html')
+	saveNotes(notes)
+	location.assign('index.html')
 })
 
 removeButton.addEventListener('click', (e) => {
-    notes.splice(noteIndex, 1) 
-    saveNotes(notes) 
-    location.assign('index.html')
+	notes.splice(noteIndex, 1)
+	saveNotes(notes)
+	location.assign('index.html')
 })
